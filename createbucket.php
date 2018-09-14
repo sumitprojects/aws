3f9910c2e872444ec57a58dd05b2a98cd109ede8
sumitmore <36819982+sumitprojects@users.noreply.github.com>
2018-09-14 14:51:56 +0530

    Update createbucket.php

--- createbucket.php
+++ createbucket.php
@@ -5,7 +5,7 @@
 include 'connecttoaws.php';
 
 // Create a unique bucket name
-$bucket = uniqid("acloudguru", true);
+$bucket = uniqid("iihtcloud", true);
 
 // Create our bucket using our unique bucket name
 $result = $client->createBucket(array(
@@ -14,7 +14,7 @@ $result = $client->createBucket(array(
 
 //HTML to Create our webpage
 echo "<h1 align=\"center\">Hello Cloud Guru!</h1>";
-echo "<div align = \"center\"><img src=\"https://acloud.guru/images/logo-small-optimised.png\"></img></div>";
+echo "<div align = \"center\"><img src=\"https://iihtsrt.com/wp-content/uploads/2017/12/logo1.png\"></img></div>";
 echo "<h2 align=\"center\">You have successfully created a bucket called {$bucket}</h2>";
 echo "<div align=\"center\"><a href=\"createfile.php?bucket=$bucket\">Click Here to Continue</a></div>";
 ?>
